15d1848e5fb276539175ec8bf9fde808e731c40d
your <omar.hasan3894@gmail.com>
2023-12-10 02:19:20 +0200

    101223

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -1,10 +1,12 @@
 ﻿const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const Joi = require('joi');
 const validateRequest = require('_middleware/validate-request');
 const authorize = require('_middleware/authorize')
 const Role = require('_helpers/role');
 const accountService = require('./account.service');
+
 router.use(cors());
 
 
