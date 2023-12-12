5e360683e17ab59f4813dcf7dcddef253862bc39
your <omar.hasan3894@gmail.com>
2023-12-12 22:17:54 +0200

    121223

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -240,7 +240,7 @@ function _delete(req, res, next) {
 function setTokenCookie(res, token) {
     // create cookie with refresh token that expires in 7 days
     const cookieOptions = {
-        httpOnly: false,
+        httpOnly: true,
         expires: new Date(Date.now() + 7*24*60*60*1000)
     };
     res.cookie('refreshToken', token, cookieOptions);
